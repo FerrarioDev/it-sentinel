37639208ba2b67a858412166a4d1c6507d3439ff
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-30 11:48:49 -0300

    feat(backend setUp, asset model):

---
+++ sentinel/assets/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class DeviceType(models.TextChoices):
+    DESKTOP = 'desktop', 'Desktop'
+    LAPTOP = 'laptop', 'Laptop'
+
+class Offices(models.TextChoices):
+    CONTROLDEGESTION = 'control de gestion','Control de Gestion'
+    PRESIDENCIA = 'presidencia', 'Presidencia'
+    OFFICE = 'office','Office'
+    PLANTABAJA = 'planta baja', 'Planta Baja'
+    PANOL = 'panol', 'Panol'
+    PLANTA = 'planta', 'Planta'
+    ACEPTACION = 'aceptacion', 'Aceptacion'
+
+class Status(models.TextChoices):
+    AVAIABLE = 'avaiable', 'Avaiable'
+    HOMEOFFICE = 'home office', 'Home office'
+    ASIGNED = 'asigned', 'Asigned'
+    TRAVEL = 'travel', 'Travel'
+    DISPOSED = 'disposed', 'Disposed'
+
+class DeviceModel(models.Model):
+    model = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    image = models.ImageField(upload_to='src/img/models')
+
+    def __str__(self):
+        return self.model
+    
+class Asset(models.Model):
+    id = models.CharField(primary_key=True, max_length=15)
+    model = models.ForeignKey(DeviceModel, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    asset_number = models.IntegerField()
+    serial_number = models.CharField(max_length=255)
+    drive_serialnumber = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=50,
+        choices=Status.choices,
+        default=Status.AVAIABLE
+    )
+    location = models.CharField(
+        max_length=50,
+        choices=Offices.choices,
+        default=Offices.OFFICE
+    )
+    device_type = models.CharField(
+        max_length=10,
+        choices=DeviceType.choices,
+        default=DeviceType.DESKTOP,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
