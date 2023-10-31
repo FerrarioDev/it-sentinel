ddc8349a4471fef054400d8c95a05764614a76be
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-31 12:34:48 -0300

    test(update detailview): doesnt work yet

--- frontend/src/components/AssetDetail.js
+++ frontend/src/components/AssetDetail.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-const AssetDetail = ({ asset }) => {
+
+const AssetDetail = ({ match }) => {
+  const [asset, setAsset] = useState();
+  const [formData, setFormData] = useState();
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/${match.params.pk}/')
+    .then(response => {
+      setAsset(response.data)
+    })
+    .catch(e => {
+      console.error('Error fetching asset:', e)
+    });
+  }, [match.params.pk])
+  
+  const handleFormChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  const handleFormSubmit = () => {
+    axios.put('http://127.0.0.1:8000/api/${match.params.pk}/', formData)
+    .then(response => {
+      console.log('Asset updated:', response.data)
+    })
+    .catch(e => {
+      console.error('Error updating asset:', e);
+    });
+  }
+
   return (
     <div>
-      <h2>Asset Details</h2>
-      <p>ID: {asset.id}</p>
-      <p>Model: {asset.model || 'N/A'}</p>
-      <p>User: {asset.user || 'N/A'}</p>
-      <p>Asset Number: {asset.asset_number || 'N/A'}</p>
-      <p>Serial Number: {asset.serial_number || 'N/A'}</p>
-      <p>Drive Serial Number: {asset.drive_serialnumber || 'N/A'}</p>
-      <p>Status: {asset.status || 'N/A'}</p>
-      <p>Location: {asset.location || 'N/A'}</p>
-      <p>Device Type: {asset.device_type || 'N/A'}</p>
-    </div>
+    <h1>Asset Detail</h1>
+    <p>ID: {asset.id}</p>
+    <form onSubmit={handleFormSubmit}>
+        <input type="text" name="user" value={formData.user || asset.user} onChange={handleFormChange} />
+        <input type='choicefield' name="status" value={formData.status || asset.status} onChange={handleFormChange} />
+        <input type="choicefield" name="location" value={formData.location || asset.location} onChange={handleFormChange} />
+        <button type="submit">Update Asset</button>
+    </form>
+</div>
   );
 };
 
