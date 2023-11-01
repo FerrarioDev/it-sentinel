0ed6849c40a6c6e408189b87ffaeaedf61efe23a
Geronimo/Denso <ferrario.dev@gmail.com>
2023-11-01 12:21:40 -0300

    feat(users/user models): user model, and userManager functionalities created

--- sentinel/users/models.py
+++ sentinel/users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, dnarID, email, password=None, **extra_fields):
+        if not dnarID:
+            raise ValueError('The ID field must be set')
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, dnarID, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(dnarID, email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    dnarID  = models.CharField(unique=True, max_length=9)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'dnarID'
+    REQUIRED_FIELDS = ['email','first_name', 'last_name']
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='custom_user_set',
+        help_text='The groups this user belongs to.',
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='custom_user_set',
+        help_text='Specific permissions for this user.',
+    )
